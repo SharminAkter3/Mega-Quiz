053062a30fee431f030f50fe379c55a334b133ba
SharminAkter3 <mssharmin4145@gmail.com>
2022-10-12 01:40:45 -0700

    added-statistics-page

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css'
-// import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
-    // const [open, setOpen] = useState(false);
     return (
         <nav className='md:flex header'>
             <div>
--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,94 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import Chart from "react-apexcharts";
+import React, { useContext, PureComponent } from 'react';
+import { ItemContext } from '../Main/Main';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 
 const Statistics = () => {
 
-    const [quizName, setQuizName] = useState([]);
-    const [quizMark, setQuizMark] = useState([]);
-
-    useEffect(() => {
-
-        const totalQuizName = [];
-        const totalQuizMark = [];
-
-        const getQuizRecord = async () => {
-            const dataMark = await fetch("https://openapi.programming-hero.com/api/quiz");
-            const dataRes = await dataMark.json();
-            console.log(dataRes);
-
-            for (let i = 0; i < dataRes.length; i++) {
-                totalQuizName.push(dataRes[i].data.name);
-                console.log(totalQuizName);
-                totalQuizMark.push(dataRes[i].data.total);
-
-            }
-            setQuizName(quizName);
-            setQuizMark(quizMark);
-        }
-        getQuizRecord();
-
-    }, []);
+    const quizTopics = useContext(ItemContext);
+    const { data } = quizTopics;
 
     return (
-        <React.Fragment>
-            <div className="container-fluid mb-5">
-                <h3 className="text-center mt-3 mb-3">Bar Chart in Quiz</h3>
 
-                <Chart
-                    type="bar"
-                    width={1380}
-                    height={700}
-                    series={[
-                        {
-                            name: "Quiz Mark",
-                            data: quizMark,
-                        },
-                    ]}
-                    options={{
-                        colors: ["#f90000"],
-                        theme: { mode: "light" },
-
-                        xaxis: {
-                            tickPlacement: "on",
-                            categories: quizName,
-                            title: {
-                                text: 'Quiz Topics',
-                                style: { color: "#f90000", fontSize: 30 },
-                            },
-                        },
-
-                        yaxis: {
-                            labels: {
-                                formatter: (quiz) => {
-                                    return `${quiz.data.name}`;
-                                },
-                                style: { fontSize: "15", colors: ["#f90000"] },
-                            },
-                            title: {
-                                text: "User In (K)",
-                                style: { color: "#f90000", fontSize: 15 },
-                            },
-                        },
-
-                        legend: {
-                            show: true,
-                            position: "right",
-                        },
-
-                        dataLabels: {
-                            formatter: (quiz) => {
-                                return `${quiz.data}`;
-                            },
-                            style: {
-                                colors: ["#f4f4f4"],
-                                fontSize: 15,
-                            },
-                        },
+        <div style={{ width: "80%", margin: "50px auto" }}>
+            <ResponsiveContainer width="100%" aspect={3}>
+                <LineChart width={400} height={350}
+                    data={data.map(data => data)}
+                    margin={{
+                        top: 5,
+                        right: 20,
+                        left: 20,
+                        bottom: 5,
                     }}
-                ></Chart>
-            </div>
-        </React.Fragment>
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="total" stroke="#8884d8" activeDot={{ r: 8 }} />
+                </LineChart>
+
+            </ResponsiveContainer>
+        </div>
+
     );
-}
+};
 
 export default Statistics;
